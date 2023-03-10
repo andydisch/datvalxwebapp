5a08b75348f8cae968cda7dd103f4569fd15ad82
adisch <andy12@gmx.ch>
2023-03-10 14:52:17 +0100

    numpy version added

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -57,10 +57,10 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS=True
-# CORS_ALLOWED_ORIGINS = [
-#     'http://127.0.0.1:3000',
-#     'http://localhost:3000'
-# ]
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:3000',
+    'http://localhost:3000'
+]
 
 ROOT_URLCONF = "backend.urls"
 
